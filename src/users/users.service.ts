8ccfdfce7d0739080a4874fab37267a0af2dea97
amandaregina <amandaregina.s@hotmail.com>
2023-11-09 17:26:29 -0300

    create a user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UsersService {
-    repo: Repository<User>
-    
-    constructor(repo: Repository<User>) {
-        this.repo = repo
+    constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+    create(email: string, password: string){
+        const user = this.repo.create({email, password})
+
+        return this.repo.save(user)
     }
 }
