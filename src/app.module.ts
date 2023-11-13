a56ee50d8512612bff6c13b8230e9b41a1862604
amandaregina <amandaregina.s@hotmail.com>
2023-11-13 18:45:37 -0300

    integration testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,9 @@ import { Report } from './reports/report.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
 import { User } from './users/user.entity';
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
+const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
@@ -19,6 +21,21 @@ import { Module } from '@nestjs/common';
     ReportsModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true
+      })
+    }
+  ],
 })
-export class AppModule {}
+
+export class AppModule {
+  configure(consumer: MiddlewareConsumer){
+    consumer.apply(cookieSession({
+      keys: ['test123456']
+    })).forRoutes('*');
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,13 @@
 import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, Session, UseGuards } from '@nestjs/common';
-import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { CurrentUser } from './decorators/current-user-decorator';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { AuthGuard } from '../guards/auth.guard';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { UserDto } from './dtos/user.dto';
 import { User } from './user.entity';
-import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
